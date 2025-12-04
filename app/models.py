c5e6a1f8ae15f24442ff9c2d06225637010d9936
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-04 17:42:33 +0300

    region-class-watershed-back-populate-relationship

--- app/models.py
+++ app/models.py
@@ -13,3 +13,33 @@ class LandUse(Base):
     watersheds = relationship ("Watershed", back_populates="land_use")
 
 
+
+class SoilType(Base):
+    __tablename__ = "soil_type"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    hydrologic_soil_group = Column(String)
+    watersheds = relationship("Watershed", back_populates="soil") 
+
+
+class Region(Base):
+    __tablename__ = "region"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    watersheds = relationship("Watershed", back_populates="region")
+
+class Watershed(Base):
+    __tablename__ = "watershed"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    area_hectares = Column(Float, nullable=False)
+    land_use_id = Column(Integer, ForeignKey("land_use.id"))
+    soil_type_id = Column(Integer, ForeignKey("soil_type.id"))
+    region_id = Column(Integer, ForeignKey("region.id"))
+
+    land_use = relationship("LandUse", back_populates="watersheds")
+    soil = relationship("SoilType", back_populates="watersheds")
+    region = relationship("Region", back_populates="watersheds")
+    rainfall_events = relationship("RainfallEvent", back_populates="watershed")
+
+
